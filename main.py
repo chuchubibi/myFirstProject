ec5aa0412b26f1b4364bdcd6bd9809a24d2b0873
sherace0311 <113529902+chuchubibi@users.noreply.github.com>
2023-02-18 01:57:41 +0000

    added data

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'manila',
+    'salary': '$500'
+  },
+  {
+    'id': 2,
+    'title': 'engineer',
+    'location': 'laguna',
+    'salary': '$400'
+  },
+{
+    'id': 3,
+    'title': 'staff',
+    'location': 'remote',
+    
+  }
+]
+
+
 @app.route("/")
 def hello():
-  return "hello,sherwin"
+  return render_template('home.html', jobs = JOBS, company_name = 'sherwin')
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
